e9e2c41225208f9c245fa51af53342a14bd5452c
YordyDev <YordyDev@gmail.com>
2025-07-02 23:38:56 -0500

    create file module

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,23 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UploadedFile, UseInterceptors, BadRequestException, NotFoundException, Res } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { fileFilter } from './helpers/fileFilter.helper';
+import { fileNamer } from './helpers/fileNamer.helper';
+import { diskStorage } from 'multer';
+import { Response } from 'express';
+import { ConfigService } from '@nestjs/config';
 
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(private readonly filesService: FilesService,
+    private readonly configService: ConfigService
+  ) { }
 
   @Post('incidencia/images')
   @UseInterceptors(FileInterceptor('file', {
-    fileFilter: fileFilter
+    fileFilter: fileFilter,
+    storage: diskStorage({
+      destination: './static/images',
+      filename: fileNamer
+    })
   }))
-  uploadIncidenciaImage( @UploadedFile() file: Express.Multer.File){
+  uploadIncidenciaImage(@UploadedFile() file: Express.Multer.File) {
 
-    if(!file) {
+    if (!file) {
       throw new BadRequestException('File is empty')
     }
+      const secureUrl = `${this.configService.get('HOST_API')}/files/incidencia/${file.filename}`
+    return secureUrl;
+  }
 
-    return file;
+  @Get('/incidencia/:imageName')
+  findProductImage(@Res() res: Response, @Param('imageName') imageName: string) {
+    console.log(imageName);
+    const path = this.filesService.getStaticIncidenciaImage(imageName);
+
+    res.sendFile(path);
+
+    // res.status(403).json({
+    //   ok:false,
+    //   path:path
+    // })
+
   }
 }
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,5 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
+
+    getStaticIncidenciaImage (imageName: string){
+        const path = join(__dirname, '../../static/images', imageName);
+        console.log(path)
+        if(!existsSync(path))
+            throw new BadRequestException(`No incidencia found with image ${imageName}`);
+
+        return path;
+    }
+
 }
